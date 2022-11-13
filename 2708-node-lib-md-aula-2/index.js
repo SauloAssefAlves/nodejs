9df9286a577093a0b0ae96de32c186ef07d1c549
SauloAssefAlves <saulo.assef98@gmail.com>
2022-11-13 16:14:04 -0300

    Lendo arquivo e utilizadno regex para extrair informações

--- 2708-node-lib-md-aula-2/index.js
+++ 2708-node-lib-md-aula-2/index.js
@@ -1,16 +1,16 @@
 import fs from "fs";
 import chalk from "chalk";
 
-const textTest =
-  "São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.).";
-
 function extrairLinks(texto) {
   const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
-  const capturas = regex.exec(texto);
-  console.log(capturas);
+
+  const capturas = [...texto.matchAll(regex)];
+  const resultados = capturas.map((cap) => ({
+    [cap[1]]: cap[2],
+  }));
+  return resultados;
 }
 
-extrairLinks(textTest);
 function trataErro(erro) {
   throw new Error(chalk.red(erro.code, "Não há arquivo no diretório"));
 }
@@ -20,7 +20,7 @@ async function pegaArquivo(caminhoArquivo) {
   try {
     const encoding = "utf-8";
     const texto = await fs.promises.readFile(caminhoArquivo, encoding);
-    console.log(chalk.green(texto));
+    console.log(extrairLinks(texto));
   } catch (erro) {
     trataErro(erro);
   } finally {
@@ -28,5 +28,6 @@ async function pegaArquivo(caminhoArquivo) {
   }
 }
 
+pegaArquivo("./arquivos/texto.md");
 
 // \[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)
