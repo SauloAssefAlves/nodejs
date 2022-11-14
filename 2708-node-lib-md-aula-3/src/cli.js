0d7886f896978a2dbe840769f72feffc9ad040ab
SauloAssefAlves <saulo.assef98@gmail.com>
2022-11-14 14:44:51 -0300

    processando diretorios e tratando erros

--- 2708-node-lib-md-aula-3/src/cli.js
+++ 2708-node-lib-md-aula-3/src/cli.js
@@ -1,13 +1,39 @@
 import chalk from "chalk";
+import fs from "fs";
 import pegaArquivo from "./index.js";
-
 
 const caminho = process.argv;
 
-async function processaTexto(caminho){
-    const resultado = await pegaArquivo(caminho[2])
-    console.log(chalk.yellow('lista de links'),resultado)
+function imporimeLista(result, identificador = "") {
+  console.log(
+    chalk.yellow("lista de arquivos"),
+    chalk.black.bgGreen(identificador),
+    result
+  );
 }
 
-processaTexto(caminho)
+async function processaTexto(argumentos) {
+  const caminho = argumentos[2];
 
+  try {
+    fs.lstatSync(caminho);
+  } catch (erro) {
+    if (erro.code === "ENOENT") {
+      console.log(chalk.red("Arquivo ou diretorio não existe"));
+      return;
+    }
+  }
+
+  if (fs.lstatSync(caminho).isFile()) {
+    const resultado = await pegaArquivo(caminho);
+    imporimeLista(resultado);
+  } else if (fs.lstatSync(caminho).isDirectory()) {
+    const arquivos = await fs.promises.readdir(caminho);
+    arquivos.forEach(async (nomeDeArquivo) => {
+      const lista = await pegaArquivo(`${caminho}/${nomeDeArquivo}`);
+      imporimeLista(lista, nomeDeArquivo);
+    });
+  }
+}
+
+processaTexto(caminho);
